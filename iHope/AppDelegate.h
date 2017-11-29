d13e4ffdc8030e200501deea43a413bbfe229ba5
harikrishna patel <nimisha@softqube.co.in>
2017-11-29 16:42:34 +0530

    Initial Commit

---
+++ iHope/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iHope
+//
+//  Created by harikrishna patel on 29/11/17.
+//  Copyright © 2017 Softqube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iHope/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iHope
+//
+//  Created by harikrishna patel on 29/11/17.
+//  Copyright © 2017 Softqube. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
